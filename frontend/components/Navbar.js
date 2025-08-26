0e2f2b140d83e19e312357c5ff7a62fd2d240411
Shadab <sadabansari451@gmail.com>
2025-08-27 03:05:05 +0530

    done

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,14 +1,17 @@
+"use client";
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center px-6 py-4 shadow-md">
       <h1 className="font-bold text-lg">Fake Documents Detection Web App</h1>
       <div className="space-x-6">
-        <a href="/" className="hover:text-blue-600">
+        <Link href="/" className="hover:text-blue-600">
           Home
-        </a>
-        <a href="/about" className="hover:text-blue-600">
+        </Link>
+        <Link href="/about" className="hover:text-blue-600">
           About
-        </a>
+        </Link>
       </div>
     </nav>
   );
